162a84f6992cfe0fcbf525993033b4f4825814d0
Geo Galagaran <17492631+geeofree@users.noreply.github.com>
2024-02-07 09:45:39 +0800

    fix: make 2x2 grid layout for the operator selection page

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,21 @@
 import {
   Button,
-  ButtonText,
   Card,
   Center,
   Heading,
   VStack,
+  ButtonIcon,
+  Box,
 } from "@gluestack-ui/themed";
 import { Link } from "expo-router";
 import { Dimensions } from "react-native";
+import { Plus, Minus, Asterisk, Divide } from "lucide-react-native";
 
 const OPERATIONS = [
-  { operation: "addition", label: "Addition" },
-  { operation: "subtraction", label: "Subtraction" },
-  { operation: "multiplication", label: "Multiplication" },
-  { operation: "division", label: "Division" },
+  { icon: Plus, operation: "addition" },
+  { icon: Minus, operation: "subtraction" },
+  { icon: Asterisk, operation: "multiplication" },
+  { icon: Divide, operation: "division" },
 ];
 
 export default function Home() {
@@ -22,17 +24,27 @@ export default function Home() {
       <Card width={Dimensions.get("window").width - 32}>
         <VStack space="md">
           <Heading textAlign="center">Math Drills</Heading>
-          {OPERATIONS.map((operation) => (
-            <Link
-              href={`/drills/${operation.operation}`}
-              key={operation.operation}
-              asChild
-            >
-              <Button>
-                <ButtonText>{operation.label}</ButtonText>
-              </Button>
-            </Link>
-          ))}
+          <Box flexDirection="row" flexWrap="wrap">
+            {OPERATIONS.map((operation, index) => (
+              <Box
+                flexDirection="row"
+                key={operation.operation}
+                width="50%"
+                justifyContent={index % 2 ? "flex-start" : "flex-end"}
+                padding="$2"
+              >
+                <Link
+                  href={`/drills/${operation.operation}`}
+                  key={operation.operation}
+                  asChild
+                >
+                  <Button>
+                    <ButtonIcon as={operation.icon} />
+                  </Button>
+                </Link>
+              </Box>
+            ))}
+          </Box>
         </VStack>
       </Card>
     </Center>
